520a638b75d8abd4db5d3bbdf6c862a9ea729dfa
Fadwa EL KHADDAR <94709894+Fadwa7@users.noreply.github.com>
2024-04-18 15:41:45 +0200

    comment in eng

--- download.sh
+++ download.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-# Vérification du nombre d'arguments
+# Verification of argument number
 if [ $# -lt 2 ]; then
     echo "Usage: $0 <ACCESSIONS_LIST> <DESTINATION_DIRECTORY>"
     exit 1
 fi
 
-# Récupération des arguments
+# Arguments
 ACCESSIONS_LIST=$1
 DESTINATION_DIRECTORY=$2
 
-# Vérification de l'existence du fichier d'accessions
+# Checking the existence of paths 
 if [ ! -f "$ACCESSIONS_LIST" ]; then
     echo "The file $ACCESSIONS_LIST is not found"
     exit 1
 fi
 
 
-# Création du répertoire de destination s'il n'existe pas
 if [ ! -d "$DESTINATION_DIRECTORY" ]; then
     echo "Please identify the output directory"
 fi 
 
 
 
-# Téléchargement des fichiers depuis S3
-cat "$ACCESSIONS_LIST" | xargs -I{} aws s3 cp s3://logan-pub/u/{}/{}.unitigs.fa.zst "$DESTINATION_DIRECTORY" --no-sign-request
+# Downloading files from S3
+cat "$ACCESSIONS_LIST" | xargs -I{} aws s3 cp s3://logan-pub/u/{}/{}.unitigs.fa.zst "$DESTINATION_DIRECTORY" --no-sign-request 
