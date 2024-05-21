e97d155f6e312eed2e6aa3d19f0fc6012e7a205a
Fadwa EL KHADDAR <94709894+Fadwa7@users.noreply.github.com>
2024-05-21 17:45:43 +0200

    add mapping

---
+++ mapping.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+#Verfication of argument number 
+if [ $# -lt 3 ]; then 
+    echo "Usage: $0 <ACCESSIONS_LIST> <GENE_SEQUENCE> <DESTINATION_DIRECTORY>"
+    exit 1
+fi 
+
+# Arguments
+ACCESSIONS_LIST=$1 # Text file containing ID accesion for your data (data should have been published before december 2023)
+GENE_SEQUENCE=$2 # fasta sequence of gene of interest
+DESTINATION_DIRECTORY=$3 # Output Directory 
+
+
+# Checking 
+if [ ! -f "$ACCESSIONS_LIST" ]; then
+    echo "The file $ACCESSIONS_LIST is not found"
+    exit 1
+fi 
+
+if [ ! -f "$GENE_SEQUENCE" ]; then 
+    echo "The file $GENE_SEQUENCE is not found"
+    exit 1
+fi 
+
+if [ ! -d "$DESTINATION_DIRECTORY" ]; then
+    echo "Please identify the output directory"
+    exit 1
+fi 
+
+# Loop over the ID 
+while IFS= read -r ID; do
+    echo "Procssing sample : $ID"
+    echo ""
+    /opt/minimap2/bin/minimap2-2.17-r941 -t 8 -a "$GENE_SEQUENCE" <(aws s3 cp s3://logan-pub/c/"$ID"/"$ID".contigs.fa.zst --no-sign-request - | zstdcat) | samtools view -hF4 - > "$DESTINATION_DIRECTORY"/"$ID".minimap2_output 
+    echo "" 
+done < "$ACCESSIONS_LIST"
