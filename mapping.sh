026ce25ccb800a85bf4750b4d2f3c3e73ab38fd2
Fadwa EL KHADDAR <94709894+Fadwa7@users.noreply.github.com>
2024-05-27 15:13:41 +0200

    Adding changes

--- mapping.sh
+++ mapping.sh
@@ -30,8 +30,22 @@ fi
 
 # Loop over the ID 
 while IFS= read -r ID; do
+<<<<<<< HEAD
+    if  [ -f "$DESTINATION_DIRECTORY/$ID.minimap2_output" ]; then
+       echo " The $ID is already processed. Skipping "
+    else
+       if aws s3 ls "s3://logan-pub/c/$ID/" --no-sign-request > /dev/null 2>&1; then
+          echo "Procssing sample : $ID"
+          echo ""
+          /opt/minimap2/bin/minimap2-2.17-r941 -t 8 -a "$GENE_SEQUENCE" <(aws s3 cp s3://logan-pub/c/"$ID"/"$ID".contigs.fa.zst --no-sign-request - | zstdcat) | samtools view -hF4 - > "$DESTINATION_DIRECTORY"/"$ID".minimap2_output 
+       else
+          echo "ERROR: sample $ID is not found in logan bucket"
+       fi 
+    fi
+=======
     echo "Procssing sample : $ID"
     echo ""
     /opt/minimap2/bin/minimap2-2.17-r941 -t 8 -a "$GENE_SEQUENCE" <(aws s3 cp s3://logan-pub/c/"$ID"/"$ID".contigs.fa.zst --no-sign-request - | zstdcat) | samtools view -hF4 - > "$DESTINATION_DIRECTORY"/"$ID".minimap2_output 
+>>>>>>> 505776e4ab1b0a85e51a876d813a216331cfa8cd
     echo "" 
 done < "$ACCESSIONS_LIST"
